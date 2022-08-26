82349384eb37f7c431a14f87742924dd008b641c
Dakota <davisdakota58@gmail.com>
2022-08-26 19:14:42 -0400

    first commit styled ecommerce site

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Announcement from "../components/Announcement";
+import Slider from "../components/Slider";
+
+const Home = () => {
+  return (
+    <div>
+      <Announcement />
+      <Slider />
+    </div>
+  );
+};
+
+export default Home;
