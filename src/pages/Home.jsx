fbac05368dcaf03bc85d6c72edab632df4a73eac
Dakota <davisdakota58@gmail.com>
2022-08-26 19:56:43 -0400

    navbar + ann. done, next is slider

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,9 @@
 import React from "react";
 import Announcement from "../components/Announcement";
+import Categories from "../components/Categories";
+import Footer from "../components/Footer";
+import Newsletter from "../components/Newsletter";
+import Products from "../components/Products";
 import Slider from "../components/Slider";
 
 const Home = () => {
@@ -7,6 +11,10 @@ const Home = () => {
     <div>
       <Announcement />
       <Slider />
+      <Categories />
+      <Products />
+      <Newsletter />
+      <Footer />
     </div>
   );
 };
